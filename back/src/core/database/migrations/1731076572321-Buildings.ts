8403a8e553a57deb77ccf8b26f4f494fce6bf1e9
Miguel21S <leaismiguelss@gamail.com>
2024-11-08 15:41:34 +0100

    Creación: de migraciones Buildings, Diseases

---
+++ back/src/core/database/migrations/1731076572321-Buildings.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Buildings1731076572321 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "building",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "address",
+                        type: "varchar",
+                        length: "255",
+                        isNullable: false
+                    },
+                    {
+                        name: "postal_code",
+                        type: "varchar",
+                        length: "10",
+                        isNullable: false
+                    },
+                    {
+                        name: "city",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: false
+                    },
+                    {
+                        name: "province",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: false
+                    },
+                    {
+                        name: "build_type",
+                        type: "varchar",
+                        length: "50",
+                        isNullable: true
+                    },
+                    {
+                        name: "floor_number",
+                        type: "int",
+                        isNullable: true
+                    }
+                ]
+            }),
+            true
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("building");
+    }
+
+}
