0b97e1c8da311e65ac477e011398f1932801361a
Miguel21S <leaismiguelss@gamail.com>
2024-11-08 14:15:51 +0100

    Eliminación y Creación: de migraciones

---
+++ back/src/entities/models/User.ts
@@ -0,0 +1,37 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "./Role";
+
+@Entity('users')
+export class User extends BaseEntity {
+    
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({'name': 'name'})
+    username!: string;
+
+    @Column({'name': 'lastName'})
+    lastname!: string;
+
+    @Column({'name': 'date_born'})
+    date_born!: Date;
+
+    @Column({'name': 'gender'})
+    gender!: string;
+
+    @Column({'name': 'phone'})
+    phone!: string;
+
+    @Column({'name': 'email'})
+    email!: string;
+
+    @Column({'name': 'password'})
+    password!: string;
+
+    @Column({'name': 'role_id'})
+    role_id!: number;
+
+    @ManyToOne(() => Role, (role) => role.users)
+    @JoinColumn({'name': 'role_id'})
+    role!: Role;
+}
