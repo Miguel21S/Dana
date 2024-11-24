045c9159bb0cef21267f33d065278371789ae720
Miguel21S <leaismiguelss@gamail.com>
2024-11-24 04:17:52 +0100

    Create: Método updateBuilding

--- back/src/entities/buildings/buildings.ts
+++ back/src/entities/buildings/buildings.ts
@@ -43,4 +43,65 @@ const createBuilding = async (req: Request, res: Response) => {
     }
 }
 
-export { createBuilding }
+const updateBuilding = async (req: Request, res: Response) => {
+    try {
+        const {roleName} = req.tokenData;
+        const building_id  = req.params.id;
+        const { address, number_build, postal_code, city, province, quantity_apartment, floor_number, build_type } = req.body;
+
+        if(isNaN(parseInt(building_id))){
+            res.status(400).json({
+                success: false,
+                message: "Invalid building ID",
+            });
+            return;
+        }
+
+        if (roleName!== "admin") {
+            res.status(403).json({
+                success: false,
+                message: "Unauthorized access",
+            });
+            return;
+        }
+
+        const building = await Building.findOne({where: { id: parseInt(building_id) } });
+
+        if(!building) {
+            res.status(404).json({
+                success: false,
+                message: "Building not found",
+            });
+            return;
+        }
+
+        await Building.update(
+            { id: parseInt(building_id) },
+            {
+                address,
+                number_build,
+                postal_code,
+                city,
+                province,
+                quantity_apartment,
+                floor_number,
+                build_type,
+            }
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Building updated successfully",
+        });
+        
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error updating building",
+            error: error
+        });
+        
+    }
+}
+
+export { createBuilding, updateBuilding }
--- back/src/entities/buildings/router.ts
+++ back/src/entities/buildings/router.ts
@@ -5,6 +5,7 @@ import { auth } from "../../core/middleware/auth";
 
 const router = Router();
 
-router.post('/auth/admin/create', auth, building.createBuilding);
+router.post('/auth/admin/create/building', auth, building.createBuilding);
+router.put('/auth/admin/update/building/:id', auth, building.updateBuilding);
 
 export default router;
